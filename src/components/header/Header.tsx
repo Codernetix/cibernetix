280a5a3f42baf4855d890aa630c43150225a57f4
Koolxdev <koolxdev@gmail.com>
2025-03-26 21:29:34 -0500

    Se crea el menu de mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,15 +5,26 @@ export default function Header() {
   const [isToggle, setToggle] = useState(false);
 
   return (
-    <header className="flex justify-between p-4 bg-black text-white items-center">
+    <header className="h-[10vh] flex justify-between p-4 bg-black text-white items-center">
       <div className="flex items-center justify-evenly">
         {" "}
         <img src={logo} alt="Logo" className="w-[75px]" />
         <p>CIBERNETIX</p>
       </div>
-      <button className="bg-white text-black">&lt;/&gt;</button>
       <nav>
-        <ul className="w-[50vw] hidden left-0 md:relative md:flex justify-evenly text-3xl">
+        <button
+          className="bg-white md:hidden text-black"
+          onClick={() => setToggle(!isToggle)}
+        >
+          &lt;/&gt;
+        </button>
+        <ul
+          className={`w-[50vw] absolute md:relative top-[10vh] md:top-0 left-0 transition-all duration-1000 bg-black  -translate-x-full ${
+            isToggle
+              ? "translate-x-0"
+              : ""
+          }  flex flex-col md:flex-row gap-4 justify-evenly text-3xl p-8 md:p-0`}
+        >
           <li>Inicio</li>
           <li>Sobre nosotros</li>
           <li>Servicios</li>
